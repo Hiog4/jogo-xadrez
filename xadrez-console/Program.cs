3b704f4801baef332ab2c758701fb0cf0bbbb403
Maicow Buchner <maicow_fb@hotmail.com>
2023-05-24 09:29:49 -0300

    Comecando a implementar o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8,8);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
